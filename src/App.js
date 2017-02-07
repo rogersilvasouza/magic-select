0731859f3c662b4d33d0242628686d79cdd3d772
Roger Silva de Souza <rogersilvasouza@hotmail.com>
2017-02-06 22:07:58 -0200

    Adicionado required e o class

--- src/App.js
+++ src/App.js
@@ -4,13 +4,32 @@ import './App.css';
 import MagicSelect from './components/MagicSelect';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <MagicSelect />
-      </div>
-    );
-  }
+
+    render() {
+
+        return (
+
+
+
+            <div className="App">
+
+                <form>
+
+                  <MagicSelect
+                    required="false"
+                    outsideClass="form-select big md-4"
+                     />
+
+                  <input type="submit" value="send" />
+
+                </form>
+
+            </div>
+
+        );
+
+    }
+
 }
 
 export default App;
