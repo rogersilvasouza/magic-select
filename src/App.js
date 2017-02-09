2e388aadcae8a763b90bc0001189b0c4108f508c
Roger Souza <roger.souza@owinteractive.com>
2017-02-09 08:25:19 -0200

    Insert options add

--- src/App.js
+++ src/App.js
@@ -7,9 +7,20 @@ class App extends Component {
 
     render() {
 
+        var data = {
+          "data" :  [{
+                      "value" : 1013,
+                      "brief" : "MG",
+                      "raw"   : "Minas Gerais"
+                    },{
+                      "value" : 1019,
+                      "brief" : "RJ",
+                      "raw"   : "Rio de Janeiro"
+                    }]
+        };
+
+
         return (
-
-
 
             <div className="App">
 
@@ -19,6 +30,7 @@ class App extends Component {
                     required="false"
                     outsideClass="meu-formulario meu-select-small"
                     defaultOption={{ raw : 'Select', brief : 'Sel', value : 9892 }}
+                    insertOptions={data}
                      />
 
                    {/*<input type="submit" value="send" />*/}
